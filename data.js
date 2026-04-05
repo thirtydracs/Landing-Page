e22eac3ba71e8f7662a63e368aaaf98d09ded44e
thirtydracs <101961664+thirtydracs@users.noreply.github.com>
2026-04-06 06:58:53 +1000

    Update data.js

--- data.js
+++ data.js
@@ -3,5 +3,10 @@ const projects = [
     name: "Caltex D'Aguilar",
     induction: "https://show.forms.app/thirtydracs/caltexdaguilarsite-induction-1-1-2-1",
     signin: "https://show.forms.app/thirtydracs/construction-site-sign-out-caltex"
+  },
+  {
+    name: "Pearl Energy Childers",
+    induction: "https://show.forms.app/thirtydracs/caltexdaguilarsite-induction-1-1-2-1",
+    signin: "https://show.forms.app/thirtydracs/construction-site-sign-out-caltex"
   }
 ];
